2599d91c3f74f05ba4b73550431ef8a9ed57c96d
jdwyah <jdwyah@b8d5060c-ad15-0410-9d10-96fc93d4f2a4>
2006-07-28 23:01:24 +0000

    simple clean-up of commented code. sysouts
    git-svn-id: file:///a/svnrepos/hippo/code/trunk/HippoTest@45 b8d5060c-ad15-0410-9d10-96fc93d4f2a4

--- src/com/aavu/server/TopicServiceImpl.java
+++ src/com/aavu/server/TopicServiceImpl.java
@@ -11,12 +11,10 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class TopicServiceImpl extends RemoteServiceServlet implements TopicService {
 
-
 	private TopicDAO topicDAO ;
 
 	public TopicServiceImpl(){
 		System.out.println("CTOR");
-
 
 		TopicDAOdb4oImpl tDAO = new TopicDAOdb4oImpl();
 		setTopicDAO(tDAO);
@@ -31,48 +29,20 @@ public class TopicServiceImpl extends RemoteServiceServlet implements TopicServi
 	 * @gwt.typeArgs <com.aavu.client.domain.TopicGWT>
 	 */
 	public Topic[] getAllTopics(int startIndex, int maxCount) {
-		System.out.println("here!!");
 
-//		Topic t = new Topic();
-//		t.setTitle("tile");
-//		Topic[] r = new Topic[1];
-//		r[0] = t;
-//		return r;
+		List<Topic> list = topicDAO.getAllTopics();
 
+		Topic[] rtn = new Topic[list.size()];
 
-		//try{
+		for(int i=0;i<list.size();i++){				
+			rtn[i] = list.get(i);
+		}
 
-			List<Topic> list = topicDAO.getAllTopics();
-
-			//Topic[] rtn = new Topic[list.size()+1];
-//			Topic tt = new Topic();
-//			tt.setTitle("Title: "+yac);
-//			rtn[rtn.length-1] = tt;
-
-			Topic[] rtn = new Topic[list.size()];
-
-			for(int i=0;i<list.size();i++){				
-				rtn[i] = list.get(i);
-			}
-			
-
-			return rtn;
-//
-//		}catch(Exception e){
-//			Topic t = new Topic();
-//			
-//			t.setTitle("Error! "+e);			
-//			e.printStackTrace();
-//			
-//			Topic[] r = new Topic[1];
-//			r[0] = t;
-//			return r;
-//		}
-
+		return rtn;
 	}
 
 	public void save(Topic topic) {
-		
+
 		topicDAO.save(topic);
 
 	}
@@ -81,7 +51,7 @@ public class TopicServiceImpl extends RemoteServiceServlet implements TopicServi
 	public String[] match(String match) {
 
 		List<Topic> list = (topicDAO.getTopicsStarting(match));
-		
+
 		String[] rtn = new String[list.size()];
 		int i=0;
 		for(Topic t : list){
@@ -89,5 +59,5 @@ public class TopicServiceImpl extends RemoteServiceServlet implements TopicServi
 		}		
 		return rtn;		
 	}
-	
+
 }
--- src/com/aavu/server/dao/db4o/TopicDAOdb4oImpl.java
+++ src/com/aavu/server/dao/db4o/TopicDAOdb4oImpl.java
@@ -10,43 +10,11 @@ import com.db4o.query.Query;
 public class TopicDAOdb4oImpl extends Db4oDAO implements TopicDAO{
 
 
-	public TopicDAOdb4oImpl(){
-
-
-//		ObjectContainer db=null;				
-
-
-//		db=Db4o.openFile(FILENAME);
-
-//		db.close();
-//		setObjectContainer(db);
-	}
-
 	public void save(Topic t){
-		System.out.println("setting "+t.getTitle());
 
-
-
-//		long id = getDb().ext().getID(t);
-//
-//		System.out.println("id in: "+id+" saved "+t.getId());
-
-//		Topic ts = getDb().ext().getByID(t.getId());
-//		System.out.println("ts "+getDb().ext().getID(ts)+" saved "+ts.getId());
-//		ts = t;		
-//		System.out.println("ts "+getDb().ext().getID(ts)+" saved "+ts.getId());
-//		getDb().set(ts);
-
-		//getDb().ext().
-		System.out.println("pre bind"+t.getTitle());
 		getDb().ext().bind(t, t.getId());
-		System.out.println("post bind"+t.getTitle());
 		getDb().set(t);
-
-
 		closeDB();
-
-		//db.set(t);
 	}
 
 	public List<Topic> getAllTopics(){
@@ -55,12 +23,12 @@ public class TopicDAOdb4oImpl extends Db4oDAO implements TopicDAO{
 		ObjectSet result = getDb().get(Topic.class);
 
 		List<Topic> res = result;
-		for(Topic t : res){
-			t.setId(getDb().ext().getID(t));
-			System.out.println(getDb().ext().getID(t)+" "+t.getTitle());
-		}
+//		for(Topic t : res){
+//			t.setId(getDb().ext().getID(t));
+//			System.out.println(getDb().ext().getID(t)+" "+t.getTitle());
+//		}
 		closeDB();
-		//ObjectSet result=db.get(Topic.class);
+		
 		return res;
 
 	}
