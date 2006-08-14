507e79cb571c8fa26eb2faf95afe807ddc2a9f65
vpech <vpech@b8d5060c-ad15-0410-9d10-96fc93d4f2a4>
2006-08-14 16:04:00 +0000

    datepicker
    git-svn-id: file:///a/svnrepos/hippo/code/trunk/HippoTest@75 b8d5060c-ad15-0410-9d10-96fc93d4f2a4

---
+++ src/com/aavu/client/widget/datepicker/SimpleDatePicker.java
@@ -0,0 +1,181 @@
+/*
+ * Simple Date Picker Widget for GWT library of Google, Inc.
+ * 
+ * Copyright (c) 2006 Parvinder Thapar
+ * http://psthapar.googlepages.com/
+ * 
+ * This library is free software; you can redistribute 
+ * it and/or modify it under the terms of the GNU Lesser 
+ * General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or 
+ * (at your option) any later version. This library is 
+ * distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY  or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNULesser General Public License for more details. 
+ * You should have received a copy of the GNU Lesser General 
+ * PublicLicense along with this library; if not, write to the 
+ * Free Software Foundation, Inc.,  
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA  
+ */
+
+package com.aavu.client.widget.datepicker;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/*
+ * SimpleDatePicker is a date picker which sub classes
+ * DatePicker and defines a calendar popup which allows
+ * user to traverse back and forth in month and year
+ */
+public class SimpleDatePicker extends DatePicker implements ClickListener {
+
+	protected PopupPanel calendarPopup = new PopupPanel(true);
+	protected VerticalPanel vertPanel = new VerticalPanel();
+	protected CalendarPanel calendarPanel = null;
+	protected TodayPanel todayPanel = null;
+	protected CalendarTraversalPanel calendarTraversalPanel = null;
+
+	/*
+	 * Default Constructor
+	 */
+	public SimpleDatePicker(){
+		super();
+		this.init();
+		this.calendarPanel = new CalendarPanel(this);
+		this.calendarTraversalPanel = new CalendarTraversalPanel(this);
+		this.todayPanel = new TodayPanel(this);
+		this.addCalendar(this);
+	}
+	
+	/*
+	 * Overridden Constructor
+	 * 
+	 * @param String
+	 * 
+	 * Uses the name to assign to the "name"
+	 * value of the HTML textbox
+	 */
+	public SimpleDatePicker(String name){
+		this();
+		DOM.setAttribute(this.getElement(), "name", name);
+	}
+
+	/*
+	 * init
+	 * 
+	 * Does the initialization to the textbox
+	 */
+	private void init(){
+		this.setWidth(80+"px");
+		this.setStyleName("txtbox");
+		addClickListener(this);
+		addKeyboardListener(this);
+	}
+
+	/*
+	 * addCalendar
+	 * 
+	 * Adds the popup panel which displays three
+	 * main panels:
+	 * 	a. CalendarTraversalPanel: DockPanel
+	 * 	b. CalendarPanel: AbsolutePanel
+	 * 	c. TodayPanel: AbsolutePanel
+	 * 
+	 * @param DatePicker
+	 */
+	private void addCalendar(DatePicker datePicker){
+		vertPanel.add(calendarTraversalPanel);
+		vertPanel.add(calendarPanel);
+		vertPanel.add(todayPanel);
+		calendarPopup.add(vertPanel);
+	}
+
+	/*
+	 * showCalendar
+	 * 
+	 * Displays the popup calendar panel
+	 */
+	private void showCalendar(){
+		calendarPopup.show();
+		calendarPopup.setPopupPosition(this.getAbsoluteLeft(), this.getAbsoluteTop() + this.getOffsetHeight()+4);
+		calendarPopup.setHeight(120+"px");
+		calendarPopup.setWidth(165 + "px");
+		calendarPopup.setStyleName("popupPanel");		
+	}
+
+	/*
+	 * hideCalendar
+	 * 
+	 * Hides the popup calendar panel
+	 */
+	public void hideCalendar(){
+		this.calendarPopup.hide();
+	}
+
+	/*
+	 * redrawCalendar
+	 * 
+	 * Redraws the calendar panel with new month/ year 
+	 */
+	public void redrawCalendar(){
+		this.calendarPanel.redrawCalendar();
+	}
+
+	/*
+	 * Methods from AbstractUIDatePicker
+	 *
+	 */
+	public void show() {
+		this.redrawCalendar();
+		this.showCalendar();
+	}
+
+	public void hide() {
+		this.hideCalendar();
+	}
+
+	/*
+	 * Methods from ClickListener
+	 */
+	public void onClick(Widget sender) {
+		this.setSelectedDate(DateUtil.convertString2Date(this.getText()));
+		this.show();
+	}
+	
+	/*
+	 * getWeekendSelectable
+	 * Getter method for isWeekendSelectable
+	 * 
+	 * @return boolean
+	 */
+	public boolean isWeekendSelectable() {
+		return this.calendarPanel.isWeekendSelectable();
+	}
+
+	/*
+	 * setWeekendSelectable
+	 * Setter method for isWeekendSelectable
+	 * 
+	 * @param boolean
+	 */
+	public void setWeekendSelectable(boolean isWeekendSelectable) {
+		this.calendarPanel.setWeekendSelectable(isWeekendSelectable);
+	}
+	
+	/*
+	 * setDateFormat
+	 * Setter method set the format of the display date
+	 * 
+	 * @param boolean
+	 */
+	public void setDateFormat(DateFormat dateFormat) {
+		super.setDateFormat(dateFormat);
+		this.calendarPanel.setDateFormatter(this.getDateFormatter());
+		this.todayPanel.setDateFormatter(this.getDateFormatter());
+	}
+}
---
+++ src/com/aavu/client/widget/datepicker/TodayPanel.java
@@ -0,0 +1,99 @@
+/*
+ * Simple Date Picker Widget for GWT library of Google, Inc.
+ * 
+ * Copyright (c) 2006 Parvinder Thapar
+ * http://psthapar.googlepages.com/
+ * 
+ * This library is free software; you can redistribute 
+ * it and/or modify it under the terms of the GNU Lesser 
+ * General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or 
+ * (at your option) any later version. This library is 
+ * distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY  or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNULesser General Public License for more details. 
+ * You should have received a copy of the GNU Lesser General 
+ * PublicLicense along with this library; if not, write to the 
+ * Free Software Foundation, Inc.,  
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA  
+ */
+
+package com.aavu.client.widget.datepicker;
+
+import java.util.Date;
+
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+
+/*
+ * TodayPanel extends AbsolyePanel and is extemely simple panel.
+ * All it shows is a link which displays today's date in the 
+ * textbox 
+ */
+public class TodayPanel extends AbsolutePanel implements ClickListener {
+
+	DatePicker datePicker;
+	// Holds the date format needed to be displayed
+	private DateFormatter dateFormatter = new DateFormatter(DateFormatter.DATE_FORMAT_MMDDYYYY);
+	
+	/*
+	 * Default Constructor 
+	 */
+	public TodayPanel(DatePicker datePicker){
+		this.datePicker = datePicker;
+		init();
+	}
+	
+	/*
+	 * init
+	 * 
+	 * Does the initialization of the panel and all its
+	 * children widgets 
+	 */
+	public void init(){
+		Label todayLink = new Label("Today");
+		todayLink.setStyleName("todayLink");
+		todayLink.addClickListener(this);
+		this.add(todayLink);
+		this.setWidth(165 + "px");
+		this.setHeight(16+"px");
+		this.setStyleName("todayPanel");		
+	}
+	
+	/*
+	 * setDateFormatter
+	 * Getter method for DateFormat
+	 * 
+	 * @return DateFormat
+	 */
+	public DateFormatter getDateFormatter() {
+		return dateFormatter;
+	}
+	
+	/*
+	 * setDateFormatter
+	 * Setter method for DateFormat
+	 * 
+	 * @param DateFormat
+	 */
+	public void setDateFormatter(DateFormatter dateFormatter) {
+		this.dateFormatter = dateFormatter;
+	}
+
+	/*
+	 * onClick
+	 * 
+	 * Overridding method from ClickListener. Populates the
+	 * textbox with the today's date. It uses DateFormatter
+	 * Object to decide which format to be displayed.
+	 */
+	public void onClick(Widget sender) {
+        //this.datePicker.setText(DateFormatter.formatDate(new Date(), DateFormatter.MMDDYYYY));
+		this.datePicker.setText(this.dateFormatter.formatDate(new Date()));
+        this.datePicker.hide();
+	}
+
+}
