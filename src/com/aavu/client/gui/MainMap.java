e3b35fa2cbb89104c6076a86c8ea230e3eacc24d
jdwyah <jdwyah@b8d5060c-ad15-0410-9d10-96fc93d4f2a4>
2006-09-17 19:51:59 +0000

    functioning js -> flash w/ the 3 islands click on "tag board" to poke flash
    git-svn-id: file:///a/svnrepos/hippo/code/trunk/HippoTest@106 b8d5060c-ad15-0410-9d10-96fc93d4f2a4

--- src/com/aavu/client/gui/MainMap.java
+++ src/com/aavu/client/gui/MainMap.java
@@ -85,34 +85,53 @@ public class MainMap extends Composite {
 	    	
 	}-*/;
 
-
+	private String boolProp(String name,boolean val){
+		return "<property id='"+name+"'><bool>"+val+"</bool></property>";
+	}
+	private String numberProp(String name,long val){
+		return "<property id='"+name+"'><number>"+val+"</number></property>";
+	}
+	private String stringProp(String name,String val){
+		return "<property id='"+name+"'><string>"+val+"</string></property>";
+	}
+	private String islandObj(long id, String name,int size){
+		return "<object>"+numberProp("id",id)+stringProp("tag",name)+numberProp("size",size)+"</object>";
+	}
+ 
+	
+    public String getCommand(){
+    	StringBuffer sb = new StringBuffer();
+    	sb.append("<invoke name=\"initLand\" returntype=\"javascript\"><arguments>");
+    	sb.append("<number>56</number>");
+    	sb.append("<array>");     	
+     	sb.append("<property id='0'>"+islandObj(7,"Music",2)+"</property>");
+     	sb.append("<property id='1'>"+islandObj(8,"Contacts",8)+"</property>");
+     	sb.append("<property id='2'>"+islandObj(24,"Books",10)+"</property>");
+     	sb.append("</array>");
+     	sb.append("<number>6</number>");
+     	sb.append("<number>100</number>");
+     	sb.append("</arguments></invoke>");    				
+		return sb.toString();
+	}
+	
 
 	//initLand(56, [{id:7,tag:"iO",size:1},{id:8,tag:"iT",size:4},{id:24,tag:"i3",size:8}], 6, 100); 
-	native void doIslands()/*-{
+	native void doIslands(String command)/*-{
+	
+	//$doc firefox
+	//$wnd IE
 	
 	var b = $wnd["ocean"];
 	
 	alert("b: "+b);
+
+ 	alert(command);
  
-    var s = "(56, [{id:7,tag:\"iO\",size:1},{id:8,tag:\"iT\",size:4},{id:24,tag:\"i3\",size:8}], 6, 100)";
- 
- 
-    var open = "<invoke name=\"initLand\" returntype=\"javascript\"><arguments>";
- 	var s = "<int>56</int>";
- 	var s1 = "<array><int>7</int><string>name</string><int>1</int></array>";
- 	var s2 = "<int>6</int>";
- 	var s3 = "<int>100</int>";
- 	var close = "</arguments></invoke>";
- 
- 
- 	alert(open+s+s1+s2+s3+close);
- 
- 	try{
-	b.CallFunction(open+s+s1+s2+s3+close);
-    }catch(e){
-     alert("fail "+e);
-    }
-		    	
+ 		try{
+			b.CallFunction(command);
+    	}catch(e){
+     		alert("fail "+e);
+    	}		    	
 	}-*/;
 	
 	
@@ -124,7 +143,7 @@ public class MainMap extends Composite {
 
 
 	public void showTagBoard() {
-		doIslands();
+		doIslands(getCommand());
 		TagEditorWindow tw = new TagEditorWindow(hippoCache);
 		tw.setPopupPosition(100,100);
 		tw.show();
