720590ccdca209c498e10c196e3ff03632e5710f
Jeff Dwyer <Jeff Dwyer@b8d5060c-ad15-0410-9d10-96fc93d4f2a4>
2007-08-30 20:19:00 +0000

    oy. forgot this in last commit.
    git-svn-id: file:///a/svnrepos/hippo/code/trunk/HippoTest@425 b8d5060c-ad15-0410-9d10-96fc93d4f2a4

---
+++ src/main/java/com/aavu/client/domain/commands/QuickAddEntryCommand.java
@@ -0,0 +1,24 @@
+package com.aavu.client.domain.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aavu.client.domain.Entry;
+import com.aavu.client.domain.Topic;
+
+public class QuickAddEntryCommand extends SaveOccurrenceCommand {
+
+	public QuickAddEntryCommand(String title, String entryString, Topic topic) {
+		Entry e = new Entry();
+		e.setTitle(title);
+		e.setData(entryString);
+		this.occurrence = e;
+
+		List topicsToLoad = new ArrayList();
+		topicsToLoad.add(e);
+		topicsToLoad.add(topic);
+
+		setTopicIDsFromTopics(topicsToLoad);
+	}
+
+}
